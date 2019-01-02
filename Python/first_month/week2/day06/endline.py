7b2235c5725785138674383ace9f2baccf4320d1
jungeunlee95 <leeap1004@gmail.com>
2019-01-02 17:40:13 +0900

    second commit4

---
+++ Python/first_month/week2/day06/endline.py
@@ -0,0 +1,13 @@
+# import sys
+
+# for i in sts.stdin:
+#     print(i, end="")
+
+menu = ["딸기", "사과", "바나나"
+        , "망고", "포도"]
+
+for food in menu:
+     print(food, end=" ")
+
+# print("hello", end="\n")
+# print("world", end="")
