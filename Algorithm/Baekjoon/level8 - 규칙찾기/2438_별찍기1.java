35709f3fadb0333999412d6116c20320d124faf1
jungeunlee95 <leeap1004@gmail.com>
2019-02-06 19:47:48 +0900

    Baek-규칙찾기 algorithm

--- Algorithm/Baekjoon/level8 - 규칙찾기/2438_별찍기1.java
+++ Algorithm/Baekjoon/level8 - 규칙찾기/2438_별찍기1.java
@@ -2,3 +2,16 @@
 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, 
 N번째 줄에는 별 N개를 찍는 문제
 */
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
