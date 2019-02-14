e550694d1a0761fcacb4e5f7b6754ff06271f91a
jungeunlee95 <leeap1004@gmail.com>
2019-02-14 17:51:55 +0900

    stack 연습문제

--- Algorithm/알고리즘/4.stack/ch 1/괄호검사.py
+++ Algorithm/알고리즘/4.stack/ch 1/괄호검사.py
@@ -21,3 +21,23 @@ b = "((()((((()()((()())((())))))"
 
 print("괄호 짝이 맞니?",checkStack(a))
 print("괄호 짝이 맞니?",checkStack(b))
+
+
+# 2
+# stack = [0] * 100
+# top = -1
+# str = "(()()))"
+#
+# wrong = 0
+# for i in range(len(str)):
+#     if str[i] == '(':
+#         top += 1; stack[top] = str[i]
+#     elif str[i] == ')':
+#         if top == -1:
+#             wrong = 1
+#             break
+#         if stack[top] == '(':
+#             top -= 1
+#
+# if top == -1 and not wrong : print("correct!")
+# else: print("wrong!")
