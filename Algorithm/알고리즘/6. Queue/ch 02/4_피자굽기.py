367380ffb26536111c87adb5890486f1c0e51ea7
jungeunlee95 <leeap1004@gmail.com>
2019-02-26 21:54:48 +0900

    피자굽기

--- Algorithm/알고리즘/6. Queue/ch 02/4_피자굽기.py
+++ Algorithm/알고리즘/6. Queue/ch 02/4_피자굽기.py
@@ -1,34 +1,42 @@
 import sys
 sys.stdin = open("4_피자굽기", "r")
 
-def bfs(c):
-      result, idx, num = 0, 0, 1
-      q = []
-      q.append([c[idx],num])
+def BFS(p):
+      id, num = 0, 1
+
+      # 화로에 첫 번째 피자 넣기
+      q.append( [num, p[id]] )
+
+      # 화로가 빌 때까지,
       while q:
-            result = q[0][1]
-            if idx < M-1:
-                  if len(q) < N:
-                        idx += 1
+            # 지금 들어있는 피자의 num을 결과로
+            result = q[0][0]
+
+            if id < m-1: # 화로에 넣지 않은 피자가 있으면
+                  if len(q) < n: # 화로가 널널하면 피자 더 넣기
+                        id += 1
                         num += 1
-                        q.append([c[idx],num])
-                  else:
-                        q[0][0] = q[0][0]//2
-                        if q[0][0] == 0:
+                        q.append([num, p[id]])
+                  else:         # 화로가 꽉 찼으면 치즈 녹여
+                        q[0][1] = q[0][1]//2
+                        if q[0][1] == 0: # 치즈 다 녹은 피자 빼기
                               q.pop(0)
-                        else:
+                        else:            # 아직 안녹았으면 피자 돌리기
                               q.append(q.pop(0))
-            else:
-                  q[0][0] = q[0][0]//2
-                  if q[0][0] == 0:
+            else: # 화로에 피자를 다 넣었으면, 남은 피자들로
+                  q[0][1] = q[0][1]//2
+                  if q[0][1] == 0:
                         q.pop(0)
                   else:
                         q.append(q.pop(0))
       return result
 
 T = int(input())
-for test_case in range(1, T + 1):
-      N, M = map(int,input().split())
-      c = list(map(int,input().split()))
+for tc in range(1, T + 1):
+      n, m = map(int,input().split())
+      p = list(map(int,input().split()))
 
-      print(f'#{test_case} {bfs(c)}')
+      q = []
+      result = 0
+
+      print(f'#{tc} {BFS(p)}')
