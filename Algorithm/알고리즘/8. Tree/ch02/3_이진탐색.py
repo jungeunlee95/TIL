f3cb1d36a4c168a9e9afb62a69b49f321336511d
jungeunlee95 <leeap1004@gmail.com>
2019-03-05 22:09:38 +0900

    Modify Tree Algorithm

--- Algorithm/알고리즘/8. Tree/ch02/3_이진탐색.py
+++ Algorithm/알고리즘/8. Tree/ch02/3_이진탐색.py
@@ -2,12 +2,18 @@ import sys
 sys.stdin = open("3_이진탐색", "r")
 
 T = int(input())
-for t in range(1, T+1):
-    a = int(input())
+def inorder(T):
+    global num
+    if T<=n:
+        inorder(T * 2)
+        tree[T]=num
+        num+=1
+        inorder(T * 2+1)
 
-    b = [0,0]
-    for i in range(1,11):
-        for j in range(2**i):
-            b.append(2+4*(j//2))
+for t in range(1,T+1):
+    n=int(input())
+    tree=[0]*(n+1)
 
-    print(f"#{t} {a//2+1} {b[a]}")
+    num = 1
+    inorder(1)
+    print(f"#{t} {tree[1]} {tree[n//2]}")
